23ea90c7c3ecb10dc974d1e2dfdca23b897415bc
inno <lemonkanghao@gmail.com>
2023-04-23 10:34:59 +0800

    分支第二次修改

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 
-import './App.css';
-import { ApiService } from './axios/request';
-
 function App() {
-  const request=async()=>{
- const result= await  ApiService.get('test')
 
-
-  }
+  
   return (
     <div className="App">
-     <button onClick={request}>request</button>
+  
     </div>
   );
 }
